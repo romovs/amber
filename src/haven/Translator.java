665c55e7eb61a1d0e2217d861681693ea42b6703
Nikita Trophimov <n.trophimov@tools4brokers.com>
2015-10-13 14:01:38 +0400

    Fixed encoding issues

--- src/haven/Translator.java
+++ src/haven/Translator.java
@@ -60,14 +60,14 @@ public class Translator {
                 return "";
 
             is = conn.getInputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
 
             String line;
             while ((line = br.readLine()) != null) {
                 responsebody.append(line);
             }
 
-            return new String(responsebody.toString().getBytes(), "UTF-8");
+            return responsebody.toString();
         } catch (Exception ex) {
         } finally {
             try {
