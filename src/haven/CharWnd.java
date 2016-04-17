3bc4ad8c4b13debb116b1d979218d9b02221d85b
puruscor <herra.melkor@gmail.com>
2016-04-17 12:59:31 +0300

    Fix Hunger bar

--- src/haven/CharWnd.java
+++ src/haven/CharWnd.java
@@ -306,13 +306,14 @@ public class CharWnd extends Window {
         private Tex rtip = null;
 
 
+
         public Object tooltip(Coord c, Widget prev) {
-            if (rtip == null) {
-                rtip = RichText.render(String.format(
-                        Resource.getLocString(Resource.l10nLabel, "%s: %d%%\nFood efficacy: %d%%"), lbl, Math.round((lglut) * 100), Math.round(gmod * 100)), -1).tex();
-            }
-            return (rtip);
+            String add = "";
+		if (finishedTime>System.currentTimeMillis())
+			add = "\nNext level: " + Utils.timeLeft(finishedTime);
+		return RichText.render(String.format("%s: %.3f%%\nFood efficacy: %d%%"+add, lbl, lglut * 100, Math.round(gmod * 100)), -1).tex();
         }
+
     }
 
     public static class Constipations extends Listbox<Constipations.El> {
