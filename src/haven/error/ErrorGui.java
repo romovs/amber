6e01606f8f62a25db7ee330c110bb5ed401317dc
puruscor <herra.melkor@gmail.com>
2016-04-17 12:41:34 +0300

    Change error JDialog to JFrame instead to avoid being unable to use client if non critical error occurs

--- src/haven/error/ErrorGui.java
+++ src/haven/error/ErrorGui.java
@@ -26,13 +26,27 @@
 
 package haven.error;
 
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
-import java.awt.event.*;
-import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
-public abstract class ErrorGui extends JDialog implements ErrorStatus {
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
+
+public abstract class ErrorGui extends JFrame implements ErrorStatus {
     private JPanel details;
     private JButton closebtn, cbbtn;
     private JTextArea exbox;
@@ -41,7 +55,7 @@ public abstract class ErrorGui extends JDialog implements ErrorStatus {
     private boolean done;
 
     public ErrorGui(java.awt.Frame parent) {
-        super(parent, "Haven error!", true);
+        super("Haven error!");
         setMinimumSize(new Dimension(300, 100));
         setResizable(false);
         add(new JPanel() {{
